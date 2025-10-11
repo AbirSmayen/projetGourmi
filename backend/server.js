99655055bfe1d2ca7a3c4d0bba4478a82b0e049a
skillhub-backend <abirsmain57@gmail.com>
2025-10-11 23:47:20 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
-const express=require("express")
-const app=express()
-const dotenv=require("dotenv").config()
-const connectDb=require("./config/connectionDb")
+const express = require("express");
+const cors = require("cors"); 
+const dotenv = require("dotenv").config();
+const connectDb = require("./config/connectionDb");
 
-const PORT=process.env.PORT || 3000
-connectDb()
-app.use(express.json())
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use("/recipe", require("./routes/recipe"))
+//Active CORS avant toute route
+app.use(cors({ origin: "http://localhost:5173" }));
 
-app.listen(PORT,(err)=>{
-    console.log('app is listening on port ${PORT}')
-})
+app.use(express.json());
+connectDb();
+
+app.use("/recipe", require("./routes/recipe"));
+
+app.listen(PORT, (err) => {
+  if (err) console.error("Erreur serveur :", err);
+  else console.log(`App is listening on port ${PORT}`);
+});
